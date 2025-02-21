c56fc02c72f52447694076e046c90d14ed8584f0
Eng Eethar <etharibrahim999@gmail.com>
2025-02-21 18:28:22 +0200

    Test App

--- src/pages/ProductDetails/ProductDetails.jsx
+++ src/pages/ProductDetails/ProductDetails.jsx
@@ -9,6 +9,7 @@ export default function ProductDetails() {
 const [product, setproduct] = useState(null);
 const [isLoading,setIsLoading]=useState(true);
 const [AddToCartLoading, setAddToCartLoading] = useState(false)
+const [isWishlisted, setIsWishlisted] = useState(false);
 const settings = {
   dots: true,
   infinite: true,
@@ -131,14 +132,34 @@ const settings = {
                         </svg>
                         Add to Cart
                     </Button>
-          <button className="bg-gray-200 flex gap-2 items-center  text-gray-800 px-6 py-2 rounded-md hover:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-gray-500 focus:ring-offset-2">
+          {/* <button className="bg-gray-200 flex gap-2 items-center  text-gray-800 px-6 py-2 rounded-md hover:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-gray-500 focus:ring-offset-2">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
                             stroke-width="1.5" stroke="currentColor" className="size-6">
                             <path stroke-linecap="round" stroke-linejoin="round"
                                 d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z" />
                         </svg>
                         Wishlist
-                    </button>
+                    </button> */}
+                     <button
+      onClick={() => setIsWishlisted(!isWishlisted)}
+      className="bg-gray-200 flex gap-2 items-center text-gray-800 px-6 py-2 rounded-md hover:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-gray-500 focus:ring-offset-2"
+    >
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 24 24"
+        strokeWidth="1.5"
+        stroke="currentColor"
+        className="size-6"
+        fill={isWishlisted ? "red" : "none"} // Toggle fill color
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z"
+        />
+      </svg>
+      Wishlist
+    </button>
         </div>
 {/* 
         <div>
